7ffa729dff129b9092b27e2b86db6add86c4160f
jaleisehall <jaleise_hall@ucsb.edu>
2021-03-09 15:49:23 -0500

    completed documentation for plastic_table

---
+++ R/plastic_table.R
@@ -0,0 +1,12 @@
+#' @title plastic_table
+#' @description mock plastic sample data to be used as an example for the detect_plastic function
+#'
+#' @format data frame with 4 variables and 19 rows
+#' @source
+#' \describe {
+#'   \item{carbon}{number of carbon atoms}
+#'   \item{hydrogen}{number of hydrogen atoms}
+#'   \item{oxygen}{number of oxygen atoms}
+#'   \item{chlorine}{number of chlorine atoms}
+#' }
+"plastic_table"
